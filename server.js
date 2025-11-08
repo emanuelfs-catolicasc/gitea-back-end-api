721b55a653fac1c41b10c07afd3d7beefb598d26
Emanuel Ferreira da Silva <emanuelfs@weg.net>
2025-11-08 18:16:30 -0300

    Incluindo testes unitários

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
-const express = require("express")
+const express = require("express");
+
+const {
+    add,
+    subtract,
+    multiply,
+    divide,
+} = require("./functions");
 
 const app = express();
 const port = 3001;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Welcome to Calculator API");
+});
+
+app.post("/add", (req, res) => {
+    res.send(add(req.body.a, req.body.b));
+});
+
+app.post("/subtract", (req, res) => {
+    res.send(subtract(req.body.a, req.body.b));
+});
+
+app.post("/multiply", (req, res) => {
+    res.send(multiply(req.body.a, req.body.b));
+});
+
+app.post("/divide", (req, res) => {
+    res.send(divide(req.body.a, req.body.b));
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Calculator API Listening on Port ${port}`);
 });
