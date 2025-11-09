0b9a4d4807f4665d06c001e92a60cb5ea18bd891
Emanuel Ferreira da Silva <emanuelfs@weg.net>
2025-11-09 18:43:04 -0300

    Restaurando o nome da API

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const port = 3001;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Welcome to Calculator API V2");
+    res.send("Welcome to Calculator API");
 });
 
 app.post("/add", (req, res) => {
